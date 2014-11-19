ff13a4db5a6d19219c0ebd8be864d4fd669b908c
ADCZ679 <terrence.watson@bnymellon.com>
2014-11-19 14:39:54 -0500

    preparing bootstrap switch

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 /**
  * Created by ADCZ679 on 11/18/2014.
  */
-var express = require("express");
+var express = require("express")
+    , lessMiddleware = require('less-middleware')
+
+    ;
+
 var app = express();
 
+app.use(lessMiddleware(__dirname + "/public"));
 app.use(express.static(__dirname + '/public'));
 app.listen(3000);
