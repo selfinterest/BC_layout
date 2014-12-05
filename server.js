8d450cbc374334a7f29469b1a78eafee391a6f15
ADCZ679 <terrence.watson@bnymellon.com>
2014-12-05 13:23:30 -0500

    doing some stuff for openshift deployment

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
  */
 var express = require("express")
     , lessMiddleware = require('less-middleware')
-
+    , http = require('http')
     ;
 
 var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
 app.use(lessMiddleware(__dirname + "/public", {compiler: {compress: false}}));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(server_port, server_ip_address);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+http.createServer(app).listen(server_port, server_ip_address);
